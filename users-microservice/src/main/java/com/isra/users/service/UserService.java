7bc266ef8de9d0b6715616e03ad77b2dfe616958
israa790 <80363109+israa790@users.noreply.github.com>
2021-11-17 23:54:32 +0100

    Add files via upload

---
+++ users-microservice/src/main/java/com/isra/users/service/UserService.java
@@ -0,0 +1,18 @@
+package com.isra.users.service;
+
+import java.util.List;
+
+import com.isra.users.entities.Role;
+import com.isra.users.entities.User;
+
+public interface UserService {
+	User saveUser(User user);
+
+	User findUserByUsername(String username);
+
+	Role addRole(Role role);
+
+	User addRoleToUser(String username, String rolename);
+    
+	List<User> findAllUsers();
+}
